f5a26ac9c69aaac39d72cd1a820fe53ade45ea88
TapyMachine3 <TapyMachine3@TapyMachine3s-iMac.local>
2017-08-04 09:55:39 +0800

    a1

--- app_api/routes/router.js
+++ app_api/routes/router.js
@@ -9,10 +9,18 @@ var gitConfig = require('../../config_git.js'); //Call in the GitHub Config
 var git = require('../controllers/gitHub');
 
 //GitHub Update - Listen for new updates
-router.post('/github-abcd1234', git.create(gitConfig).post)
+//router.post('/github-abcd1234', git.create(gitConfig).post)
 //Strange link right? Well, it has to be unqiue and ensure that no one can guess it
 
+// GET method route
+router.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+})
 
+// POST method route
+router.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
 
 
 //Push the router to the app
