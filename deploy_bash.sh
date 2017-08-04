0af0beeff9b9358d3b46e2916f2b8a75fd30b465
TapyMachine3 <TapyMachine3@TapyMachine3s-iMac.local>
2017-08-04 14:47:45 +0800

    a1

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -8,8 +8,10 @@
 cd /home/shankqr/
 git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/flowbot.git
 pm2 delete all
-sudo rm -r /home/shankqr/www/flowbot
-sudo mv /home/shankqr/flowbot /home/shankqr/www/flowbot
+rm -r /home/shankqr/www/flowbot
+y
+y
+mv /home/shankqr/flowbot /home/shankqr/www/flowbot
 cd /home/shankqr/www/flowbot
 npm install
 pm2 start pm2config.json
