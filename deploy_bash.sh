74c9ca9479632d16c68de3188fcbacfee2131a77
TapyMachine3 <TapyMachine3@TapyMachine3s-iMac.local>
2017-08-05 15:27:39 +0800

    a1

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -1,15 +1,16 @@
+cd /home/shankqr/
+git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/flowbot.git
+pm2 delete flowbot
+sudo rm -r /home/shankqr/www/flowbot
+sudo mv /home/shankqr/flowbot /home/shankqr/www/flowbot
+cd /home/shankqr/www/flowbot
+npm install
+pm2 start index.js --name "flowbot"
+
+
 
 #Remove any previous folder
 #sudo rm -r flowbot
 
 #Change rw permision to me
 #sudo chown shankqr flowbot
-
-cd /home/shankqr/
-git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/flowbot.git
-sudo rm -r /home/shankqr/www/flowbot
-sudo mv /home/shankqr/flowbot /home/shankqr/www/flowbot
-cd /home/shankqr/www/flowbot
-npm install
-pm2 delete flowbot
-pm2 start index.js --name "flowbot"
