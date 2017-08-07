e7b8554cb8a09daf296b326cfc756160e379a542
Shankar Nathan <shankqr@gmail.com>
2017-08-07 16:53:45 +0800

    update bash to /var/www

--- deploy_autodeploy.sh
+++ deploy_autodeploy.sh
@@ -1,8 +1,8 @@
-cd
+cd /home/shankqr/
 git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/autodeploy.git
 pm2 delete autodeploy
-sudo rm -r www/autodeploy
-sudo mv autodeploy www/autodeploy
-cd www/autodeploy
+sudo rm -r /var/www/autodeploy
+sudo mv /home/shankqr/autodeploy /var/www/autodeploy
+cd /var/www/autodeploy
 npm install
 pm2 start pm2config.json
--- deploy_bash.sh
+++ deploy_bash.sh
@@ -1,16 +1,8 @@
 cd /home/shankqr/
 git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/flowbot.git
 pm2 delete flowbot
-sudo rm -r /home/shankqr/www/flowbot
-sudo mv /home/shankqr/flowbot /home/shankqr/www/flowbot
-cd /home/shankqr/www/flowbot
+sudo rm -r /var/www/flowbot
+sudo mv /home/shankqr/flowbot /var/www/flowbot
+cd /var/www/flowbot
 npm install
 pm2 start pm2config.json
-
-
-
-#Remove any previous folder
-#sudo rm -r flowbot
-
-#Change rw permision to me
-#sudo chown shankqr flowbot
