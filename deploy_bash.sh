8cd53210ea6a0df2acc55508ab6dc4097fbe9431
Shankar Nathan <shankqr@gmail.com>
2018-03-16 14:29:13 +0800

    obot folder

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -1,8 +1,8 @@
 cd /home/shankqr/
 git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/obot.git
-pm2 delete flowbot
-sudo rm -r /var/www/flowbot
-sudo mv /home/shankqr/flowbot /var/www/flowbot
-cd /var/www/flowbot
+pm2 delete obot
+sudo rm -r /var/www/obot
+sudo mv /home/shankqr/obot /var/www/obot
+cd /var/www/obot
 npm install
 pm2 start pm2config.json
