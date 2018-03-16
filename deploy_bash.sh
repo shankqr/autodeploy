9c5a196ff911c786a1a992d86daa1722b511efb5
Shankar Nathan <shankqr@gmail.com>
2018-03-16 13:46:02 +0800

    obot name change

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -1,5 +1,5 @@
 cd /home/shankqr/
-git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/flowbot.git
+git clone https://1ab2a4fa5e9da69ed7ddc578af8b47edec1b8d01@github.com/shankqr/obot.git
 pm2 delete flowbot
 sudo rm -r /var/www/flowbot
 sudo mv /home/shankqr/flowbot /var/www/flowbot
